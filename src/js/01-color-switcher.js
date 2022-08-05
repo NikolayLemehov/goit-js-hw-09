1780d79a9ac39ccd4f8c7c064bde56612a51a0cb
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-05 15:49:19 +0300

    task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const CHANGE_COLOR_INTERVAL = 1000;
+let intervalId = null;
+let isRunningChange = false;
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+}
+
+refs.startBtn.addEventListener('click', onClickStartBtn)
+refs.stopBtn.addEventListener('click', onClickStopBtn)
+
+
+function onClickStartBtn() {
+  if (isRunningChange) return;
+
+  refs.startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, CHANGE_COLOR_INTERVAL)
+}
+
+function onClickStopBtn() {
+  if (!isRunningChange) return;
+
+  clearInterval(intervalId)
+  refs.startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
