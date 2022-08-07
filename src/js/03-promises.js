1be8d8288c99c8ba1cb79ed0a43a73f252e237f6
Nikolay_Liemiekhov <nicklemster@gmail.com>
2022-08-07 15:43:00 +0300

    task-03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,46 @@
+import "notiflix/dist/notiflix-3.2.5.min.css";
+import {Notify} from 'notiflix/build/notiflix-notify-aio';
+
 const refs = {
   form: document.querySelector('form.form'),
 };
 
 refs.form.addEventListener('submit', onSubmitForm)
 
+function getFormData(formRef) {
+  return [...formRef.elements]
+    .filter(it => it.hasAttribute('name'))
+    .reduce((acc, it) => ({
+      ...acc,
+      [it.getAttribute('name')]: Number(it.value),
+    }), {})
+}
+
 function onSubmitForm(e) {
   e.preventDefault();
-  console.log(refs.form.children);
+  const {delay, step, amount} = getFormData(refs.form)
+  for (let i = 0; i < amount; i++) {
+    console.log(amount, i)
+    const promise = i === 0 ? createPromise(i, delay) : createPromise(i, delay + i * step);
+    promise
+      .then(({position, delay}) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({position, delay}) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+  }
 }
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+  })
 }
